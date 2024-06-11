56a1fbe57c1cf92faa5ea37a615d630aa56bca1e
Matt <60483021+MattMlgn@users.noreply.github.com>
2024-06-11 20:42:55 +0200

    added get_attivita_per_appelli()

--- start.py
+++ start.py
@@ -1,14 +1,15 @@
 import requests
 from requests.auth import HTTPBasicAuth
-from calls import anagrafica
+from utils import anagrafica
+
+# Definisci le credenziali
+username = 'bngnnp01t44h579x'
+password = 'NinaPia.2020'
+
+# Definisci l'URL dell'endpoint
+url = "https://unical.esse3.cineca.it/e3rest/api"
 
 def authenticate():
-    # Definisci le credenziali
-    username = 'bngnnp01t44h579x'
-    password = 'NinaPia.2020'
-
-    # Definisci l'URL dell'endpoint
-    url = "https://unical.esse3.cineca.it/e3rest/api/login"
 
     # Opzionale: aggiungi parametri alla richiesta
     params = {
@@ -16,9 +17,32 @@ def authenticate():
     }
 
     # Esegui la richiesta GET con autenticazione BASIC
-    response = requests.get(url, auth=HTTPBasicAuth(username, password), params=params)
+    response = requests.get(url+"/login", auth=HTTPBasicAuth(username, password), params=params)
     return response
 
+def get_attivita_per_appelli():
+
+    # Opzionale: aggiungi parametri alla richiesta
+    params = {
+        'start': 0,
+        'limit': 50,
+        'order': '+dataInizio'
+    }
+
+    # Esegui la richiesta GET con autenticazione BASIC
+    response = requests.get(url+"/calesa-service-v1/appelli", auth=HTTPBasicAuth(username, password), params=params)
+
+    # Verifica la risposta
+    if response.status_code == 200:
+        appelli = response.json()
+        #for appello in appelli:
+            #print(f"Data: {appello['dataInizio']}, Descrizione: {appello['descrizione']}")
+        print(appelli)
+    else:
+        print(f"Errore: {response.status_code}")
+        print(response.text)
+        
 if __name__ == "__main__":
-    response = authenticate()
-    anagrafica(response)
+    #anagrafica(authenticate())
+    #get_attivita_per_appelli()
+    pass
